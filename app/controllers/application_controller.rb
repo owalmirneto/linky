936fe428bc3708fd0a904e6fbdc653bfc4d27984
Walmir <owalmirneto@gmail.com>
2026-03-05 19:41:54 -0300

    Create authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+  # TODO: Skip authentication for now
+  # include Authentication
+
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 end
